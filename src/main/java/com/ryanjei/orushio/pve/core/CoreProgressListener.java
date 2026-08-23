53619a7536a8875302073d572feeb849d31c87b8
ryanjei <ryanjeidegoma@gmail.com>
2026-08-23 19:34:44 +0900

    Make core destruction retry safe

--- src/main/java/com/ryanjei/orushio/pve/core/CoreProgressListener.java
+++ src/main/java/com/ryanjei/orushio/pve/core/CoreProgressListener.java
@@ -1,3 +1,4 @@
 package com.ryanjei.orushio.pve.core;
 import java.util.UUID;
+/** Implementations must be idempotent because failed destruction side effects are retried. */
 public interface CoreProgressListener{default void normalCoreDestroyed(UUID sessionId,int destroyed,int required){}default void finalCoreDestroyed(UUID sessionId){}static CoreProgressListener none(){return new CoreProgressListener(){};}}
